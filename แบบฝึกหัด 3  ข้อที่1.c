8b93e31b12d0ffb04e588bc7b28a57631c0fbb7f
Kanit Wijitpanya <32562129+kkenth007@users.noreply.github.com>
2017-11-16 18:21:10 +0700

    Create แบบฝึกหัด 3  ข้อที่1.c

---
+++ แบบฝึกหัด 3  ข้อที่1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ main()
+ {
+	int x;
+	printf("Enter x: ");
+	scanf("%d",&x);
+	if(x%5==0){
+	    printf("%d is divisible by 5.");
+	}else
+	printf("%d is not divisible by 5.");                            
+ }
